32af611b13fa2bbe4a2fb8550a00c90928756945
David <80283120+Cladorhiza@users.noreply.github.com>
2024-09-29 00:58:43 +0100

    pong removed to own repository

--- VulkanDemoNew/Pong/src/components/AIControllerPaddleComponent.cpp
+++
@@ -1,7 +0,0 @@
-#include "AIControllerPaddleComponent.h"
-
-AIControllerPaddleComponent::AIControllerPaddleComponent(float topBound, float bottomBound, float paddleSpeed) 
-	:topBoundary(topBound), bottomBoundary(bottomBound), paddleSpeed(paddleSpeed)
-{
-
-}
--- VulkanDemoNew/Pong/src/components/PlayerControllerPaddleComponent.cpp
+++
@@ -1,8 +0,0 @@
-#include "PlayerControllerPaddleComponent.h"
-
-
-PlayerControllerPaddleComponent::PlayerControllerPaddleComponent(int upKey, int downKey, float paddleSpeed, float topBound, float bottomBound) 
-	:upKeyBind(upKey), downKeyBind(downKey), paddleSpeed(paddleSpeed), topBoundary(topBound), bottomBoundary(bottomBound)
-{
-
-}
